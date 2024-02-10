e0f25473c36612663ec29adcae47e8193edbb591
Blane <dawid.szoldra17@gmail.com>
2024-02-10 21:36:35 +0100

    CHANGE: Classic HUD moved to c++ WIP

---
+++ Source/PacMan/UI/HUD/PMClassicHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PMClassicHUD.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class PACMAN_API UPMClassicHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+};
+
+
