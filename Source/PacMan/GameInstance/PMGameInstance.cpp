b5d64af4c2ce67b9b7a6e3cc1c3fc894564a9262
Blane <dawid.szoldra17@gmail.com>
2024-02-19 01:07:08 +0100

    NEW: Classic 3rd level

--- Source/PacMan/GameInstance/PMGameInstance.cpp
+++ Source/PacMan/GameInstance/PMGameInstance.cpp
@@ -10,6 +10,7 @@ void UPMGameInstance::Init()
 
 	ClassicLevels.Add(1, "Classic01");
 	ClassicLevels.Add(2, "Classic02");
+	ClassicLevels.Add(3, "Classic03");
 
 	MazeLevels.Add(1, "Maze01");
 	//MazeLevels.Add(2, "Maze02");
