c5a8f8739c60761a5295a0a01e8fc9e02847f819
Blane <dawid.szoldra17@gmail.com>
2024-02-19 15:17:56 +0100

    CHANGE: Maze 2nd level - FINAL

--- Source/PacMan/GameInstance/PMGameInstance.cpp
+++ Source/PacMan/GameInstance/PMGameInstance.cpp
@@ -12,7 +12,7 @@ void UPMGameInstance::Init()
 	ClassicLevels.Add(2, "Classic02");
 	ClassicLevels.Add(3, "Classic03");
 
-	MazeLevels.Add(1, "Maze01");
+	MazeLevels.Add(1, "Maze02");
 	MazeLevels.Add(2, "Maze02");
 
 	SaveSlotName = "SaveSlot";
